51bdd30873dbca17ed962043ed949a01c7df3971
BRIAN OTIENO <osano3204@gmail.com>
2021-05-28 11:23:51 +0300

    Add files via upload
    This program approximates  amount to be received after a particular period

---
+++ bank_approximator.py
@@ -0,0 +1,33 @@
+def calcrate():
+    fname=input("Enter names :")
+    age=int(input("Enter age :"))
+    if age<=17:
+        print("Not valid for persons under 18years of age try again the the next ",18-age," years")
+        exit()
+    else:
+        year1=int(input("Enter  year of investment :"))
+        amnt=int(input("Enter Investment amount :"))
+        yearcounts1=int(input("Enter year of withdrawal :"))
+        rate = 4
+        rateyear=yearcounts1 - year1
+        interestincome1 = amnt *rate * rateyear
+        interestincome1final=interestincome1/100
+        totalamnt=amnt +interestincome1final
+        final_total=totalamnt *rate * rateyear
+        print( "Your income/profit by the end of " ,rateyear, " year(s)  provincially ,therefore "
+        "your total earnings will be ", final_total)
+
+
+print(calcrate())
+
+
+"""def circlearea( ):
+    PI=3.14285714285
+    r= int(input("Enter the radius :"))
+    h=int(input("Enter the radius :"))
+    
+    area=PI*r* r*h
+    area1="{:.2f}".format(area)
+    print("Area of circle is approximately:", area1)
+
+print(circlearea())"""
